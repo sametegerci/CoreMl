490a31aa35b4d4c1dcbad9f449811cf75d783815
samet.egerci <161208667+sametegerci@users.noreply.github.com>
2024-08-02 16:55:24 +0300

    Initial Commit

---
+++ CoreMlApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreMlApp
+//
+//  Created by  Samet Eğerci on 2.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
